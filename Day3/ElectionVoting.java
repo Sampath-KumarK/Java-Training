9529ff1e981ec834ef69459cb415e82a6c56136a
Sampath-KumarK <sampathkumark005@gmail.com>
2026-04-29 11:29:00 +0530

    Voting

---
+++ Day3/ElectionVoting.java
@@ -0,0 +1,15 @@
+package Day3;
+import java.util.*;
+public class ElectionVoting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age :");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You are eligible to vote");
+        }
+        else{
+            System.out.println("You are not eligible to vote");
+        }
+    }
+}
--- Day3/Markgrading.java
+++ Day3/Markgrading.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class coni{
+public class Markgrading{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the mark :");
