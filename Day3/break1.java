7c2c457ce41bd4587da495338a613b19c064de59
Sampath-KumarK <sampathkumark005@gmail.com>
2026-04-29 22:49:23 +0530

    Day4commit

---
+++ Day3/break1.java
@@ -0,0 +1,10 @@
+class break1{
+    public static void main(String args[]){
+        for(int i=0;i<=10;i++){
+            if(i%2 == 1){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
