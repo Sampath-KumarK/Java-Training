96501e199c88047423d690d8f0f8432ea832a2c6
Sampath-KumarK <sampathkumark005@gmail.com>
2026-04-30 09:54:57 +0530

    Task1

---
+++ Day4/Task1/PosOrNeg.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PosOrNeg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num>0){
+            System.out.println(num+" is a positive number");
+        }
+        else if(num<0){
+            System.out.println(num+" is a negative number");
+        }
+        else{
+            System.out.println(num+" is zero");
+        }
+    }
+    
+}
